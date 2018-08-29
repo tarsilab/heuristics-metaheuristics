9229f0d978a7e8c8c83b18103a44b4e1689427cf
Tarsila Bessa <tarsila.bessa@dcc.ufmg.br>
2018-08-29 15:12:08 -0300

    Implemented nearest neighbor heuristic

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 #include <cstring>
+#include <limits>
 #include "graph.h"
+
+int nearestNeighbor(Graph &g, int source) {
+	int original_source = source;
+	int numVertices = g.getNumVertices();
+	int visitedVertices = 0;
+	int newSource = 0;
+	std::vector < std::vector < std::pair<int, int> > > adjList = g.getAdjList();
+	std::vector<bool> visited(numVertices);
+
+	visitedVertices = 1;
+	int sum = 0;
+
+	while (visitedVertices != numVertices) {
+		int smallest = std::numeric_limits<int>::max();
+		visited[source] = true;
+		
+		for (auto &i : adjList[source]) {
+			if (i.second < smallest && (visited[i.first] == false)) {
+				smallest = i.second;
+				newSource = i.first;
+			}
+		}
+
+		if (source == newSource) {
+			sum = 0;
+			break;
+		}
+		
+		sum += smallest;
+		source = newSource;
+		visitedVertices++;
+	}  
+
+	if (sum != 0) {
+		for (auto &i : adjList[source])	if (i.first == original_source) sum += i.second;
+	}
+
+	return sum;
+}
 
 int main(int argc, char *argv[]) {
 
 	long long source, destination;
-	float weight;
+	int weight;
 	int vertices;
 
 	std::cin >> vertices;
@@ -16,6 +56,8 @@ int main(int argc, char *argv[]) {
 		graph.addEdge(source - 1, destination - 1, weight);
 	}
 
+	int tsp = nearestNeighbor(graph, 0) ;
+	std::cout << tsp << "\n";
 	//graph.printGraph();
 	
 	return 0;
